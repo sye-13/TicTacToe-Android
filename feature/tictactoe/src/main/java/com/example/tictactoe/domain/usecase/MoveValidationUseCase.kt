ab7d24159224b209832085a458c551e14df5cdde
dev <>
2025-05-26 11:34:09 +0200

    Implement MoveValidationUseCase to validate a move

--- feature/tictactoe/src/main/java/com/example/tictactoe/domain/usecase/MoveValidationUseCase.kt
+++ feature/tictactoe/src/main/java/com/example/tictactoe/domain/usecase/MoveValidationUseCase.kt
@@ -1,6 +1,7 @@
 package com.example.tictactoe.domain.usecase
 
 import com.example.tictactoe.domain.model.Board
+import com.example.tictactoe.domain.model.Cell
 import javax.inject.Inject
 
 sealed interface MoveValidationResult {
@@ -13,6 +14,12 @@ sealed interface MoveValidationResult {
 
 class MoveValidationUseCase @Inject constructor() {
     operator fun invoke(cellIndex: Int, board: Board): MoveValidationResult {
+        if (cellIndex !in board.cells.indices) {
+            return MoveValidationResult.Invalid.CellOutOfBound
+        }
+        if (board.cells[cellIndex] is Cell.Occupied) {
+            return MoveValidationResult.Invalid.CellAlreadyOccupied
+        }
         return MoveValidationResult.Valid
     }
 }
